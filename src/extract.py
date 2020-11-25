660539a28feb1354c75aafd2fc5a548bf2604ea8
Zack <zack@magnotti.net>
2020-11-24 20:14:35 -0800

    implemented export function

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,8 @@
 from slippi import Game
 from scipy import sparse
+from pymongo import MongoClient
+from bson.binary import Binary
+import pickle
 import numpy as np
 
 class GameAbortedError(AttributeError):
@@ -129,5 +132,27 @@ def extract(f, as_sparse=False):
         
     return tuple(out)
 
-def export(f):
-    pass
+def export(f, 
+           database_name, 
+           collection_name,
+           host = 'localhost',
+           port = 27017):
+    # Connect to the hosted MongoDB instance
+    client = MongoClient(host, port)
+    db = client[database_name]
+    collection = db[collection_name]
+    
+    players = extract(f, as_sparse=True)
+    mongo_output = []
+    for player in players:
+        sanitized = {}
+        for k, v in player.items():
+            if isinstance(v, sparse.csr.csr_matrix):
+                # if value is a sparse matrix, convert to binary
+                sanitized[k] = Binary(pickle.dumps(v, protocol=2))
+            else:
+                sanitized[k] = v
+        
+        # export data to mongodb
+        mongo_output.append(sanitized)
+    collection.insert_many(mongo_output)
