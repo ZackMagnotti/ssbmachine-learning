ab2b22646d74b1978235b234d610b6ccc8f0371e
Zack <zack@magnotti.net>
2020-12-08 22:14:43 -0800

    styling changes in generator.py

--- src/generator.py
+++ src/generator.py
@@ -14,63 +14,63 @@ def data_generator(clip_collection,
                    limit=None,
                    onehot=True):
         
-        cur = clip_collection.find()
+    cur = clip_collection.find()
+    
+    if skip:
+        cur.skip(skip)
+    
+    if limit:
+        cur.limit(limit)
         
-        if skip:
-            cur.skip(skip)
+    while cur.alive:
         
-        if limit:
-            cur.limit(limit)
-            
-        while cur.alive:
-            
-            xi = []
-            yi = []
-            
-            for _ in range(batch_size):
-                # get the next clip
-                try:
-                    for _ in range(step):
-                        try:
+        xi = []
+        yi = []
+        
+        for _ in range(batch_size):
+            # get the next clip
+            try:
+                for _ in range(step):
+                    try:
+                        clip = next(cur)
+
+                    except StopIteration:
+                        if repeat:
+                            cur = clip_collection.find()
+                            if skip:  cur.skip(skip)
+                            if limit: cur.limit(limit)
                             clip = next(cur)
-
-                        except StopIteration:
-                            if repeat:
-                                cur = clip_collection.find()
-                                if skip:  cur.skip(skip)
-                                if limit: cur.limit(limit)
-                                clip = next(cur)
-                            else:
-                                raise
-                
-                # if StopIteration, abort loop and yield what there is so far
-                # this will be the last yield
-                except StopIteration:
-                    break
-
-                # if next clip is fetched successfully append its info to the lists
-                else:
-                    if mode != 'Y' and mode != 'y':
-                        xi.append(pickle.loads(clip['istream']).toarray())
-                    yi.append(id_from_char[clip['character']])
-
-            if xi == [] and yi == []:
-                raise StopIteration
+                        else:
+                            raise
             
-            if mode != 'Y' and mode != 'y':
-                xi = np.stack(xi, axis=0)
-            
-            if onehot:
-                yi = tf.one_hot(yi, 26)
-            
-            if mode == 'X' or mode == 'x':
-                yield xi
-                
-            elif mode == 'Y' or mode == 'y':
-                yield yi
-                
-            elif mode:
-                raise ValueError("mode must be 'X' or 'Y' or None")
+            # if StopIteration, abort loop and yield what there is so far
+            # this will be the last yield
+            except StopIteration:
+                break
 
+            # if next clip is fetched successfully append its info to the lists
             else:
-                yield xi, yi
+                if mode != 'Y' and mode != 'y':
+                    xi.append(pickle.loads(clip['istream']).toarray())
+                yi.append(id_from_char[clip['character']])
+
+        if xi == [] and yi == []:
+            raise StopIteration
+        
+        if mode != 'Y' and mode != 'y':
+            xi = np.stack(xi, axis=0)
+        
+        if onehot:
+            yi = tf.one_hot(yi, 26)
+        
+        if mode == 'X' or mode == 'x':
+            yield xi
+            
+        elif mode == 'Y' or mode == 'y':
+            yield yi
+            
+        elif mode:
+            raise ValueError("mode must be 'X' or 'Y' or None")
+
+        else:
+            yield xi, yi
