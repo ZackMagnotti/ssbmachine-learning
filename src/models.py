5d1628279a2e44013f1e11ac4bd80fb9c76ef55e
Zack <zack@magnotti.net>
2020-12-30 11:22:40 -0800

    created custom_mk2

--- src/models.py
+++ src/models.py
@@ -2,7 +2,8 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv1D, SpatialDropout1D
-from tensorflow.keras.layers import MaxPooling1D, GlobalAveragePooling1D
+from tensorflow.keras.layers import MaxPooling1D, GlobalMaxPooling1D
+from tensorflow.keras.layers import GlobalAveragePooling1D
 from tensorflow.keras.activations import swish
 
 top_8_accuracy = keras.metrics.TopKCategoricalAccuracy(k=8, name='top 8 accuracy')
@@ -245,7 +246,6 @@ def vgg_type(activation=swish,
     
     return model
 
-
 def custom_type(activation=swish,
                 loss=focal_loss,
                 optimizer='adam',
@@ -317,3 +317,60 @@ def custom_type(activation=swish,
     
     return model
 
+def custom_mk2(activation=swish,
+               loss=focal_loss,
+               optimizer='adam',
+               name='custom_mk2'):
+
+    model = Sequential(name=name)
+
+    model.add(Input(shape=(None, 13)))
+
+    # -----------------------------------------------
+    # number of filters: 128
+    # size of filters:   30
+    # sees: .5s
+
+    model.add(Conv1D(150, 30, activation=activation, name='conv1'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    # -----------------------------------------------
+    # number of filters: 128
+    # size of filters:   15
+    # sees: .5s
+
+    model.add(Conv1D(128, 15, activation=activation, name='conv2.1'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    # -----------------------------------------------
+    # number of filters: 256
+    # size of filters:   15
+    # sees: 1s
+    model.add(Conv1D(128, 15, activation=activation, name='conv3.1'))
+    model.add(Conv1D(128, 15, activation=activation, name='conv3.2'))
+    model.add(MaxPooling1D(pool_size=4))
+
+    # -----------------------------------------------
+    # number of filters: 256
+    # size of filters:   15
+    # sees: 4s
+    model.add(Conv1D(256, 15, activation=activation, name='conv4'))
+
+    # sees whole clip, takes pool
+    model.add(GlobalMaxPooling1D())
+    model.add(Flatten())
+
+    model.add(Dense(80, activation=activation, name='dense1'))
+    model.add(Dropout(.2))
+
+    model.add(Dense(80, activation=activation, name='dense2'))
+    model.add(Dropout(.2))
+
+    # final output layer
+    model.add(Dense(26, activation='softmax', name='final'))
+
+    model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=['accuracy', top_8_accuracy])
+    
+    return model
