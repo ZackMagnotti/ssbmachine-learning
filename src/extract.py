122e54de17ea72d562411eae2ed2c3c586fd9e6c
Zack <zack@magnotti.net>
2021-01-08 20:11:10 -0800

    messy syntax I cleaned up was actually important so I put it back

--- src/extract.py
+++ src/extract.py
@@ -85,10 +85,11 @@ def get_istreams(game, as_sparse=True):
         # if as_sparse is true and port is active, 
         # convert to compressed sparse array
         # else convert to numpy array
-        if as_sparse:
-            istream = csr_matrix(istream)
         else:
-            istream = istream.toarray()
+            if as_sparse:
+                istream = csr_matrix(istream)
+            else:
+                istream = istream.toarray()
             
         istreams.append(istream)
     
