89456f9c95565b1ab5e1dff0032ea05e95bf18b5
Zack <zack@magnotti.net>
2021-01-09 16:00:03 -0800

    removed depreciated code

--- src/export.py
+++ src/export.py
@@ -12,29 +12,6 @@ from .util import display_progress
 class PathError(ValueError):
     pass
 
-# def display_progress(i, N):
-#     ''' 
-#     For convenience, to see progress
-#     when exporting large directories.
-
-#     Parameters
-#     -----------
-#     i (int) : current iteration
-#     N (int) : total number of iterations in process
-#     '''
-
-#     bar_length = 20
-
-#     progress = (bar_length * i) // N
-#     progress_percent =  round(100 * i / N, 2)
-
-#     progress_bar = ('#' * progress) 
-#     progress_bar += ('.' * (bar_length - progress))
-#     progress_bar = '[' + progress_bar + ']'
-
-#     stdout.write(f'\r{progress_bar} {i} of {N} - {progress_percent}% ')
-#     stdout.flush()
-
 def export(f, 
            database_name, 
            collection_name,
