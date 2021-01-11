7cad631ab1e5a0693406100bdba4f7eef535313a
zack ec2 <zack@magnotti.net>
2021-01-11 22:41:00 +0000

    created src.transfer.standard_head and made src.transfer.add_new_head better

--- src/transfer.py
+++ src/transfer.py
@@ -1,4 +1,23 @@
 from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Dropout
+from tensorflow.keras.activations import swish
+
+standard_head = [
+    # dense layer 1
+    Dense(128, name='head_dense_1')
+    BatchNormalization(name='head_batchnorm_1')
+    Activation(swish, name='head_activation_1')
+    Dropout(.5, name='head_dropout_1')
+    
+    # dense layer 2
+    Dense(128, name='head_dense_2')
+    BatchNormalization(name='head_batchnorm_2')
+    Activation(swish, name='head_activation_2')
+    Dropout(.5, name='head_dropout_2')
+    
+    # final output layer
+    Dense(1, activation='sigmoid', name='output')
+]
 
 def remove_head(base_model, trainable=False):
     # use flatten layer to 
@@ -14,8 +33,12 @@ def remove_head(base_model, trainable=False):
     headless_base_model.trainable = trainable
     return headless_base_model
 
-def add_new_head(base_model, head):
-    return Sequential([base_model, head])
+def add_new_head(model, head=standard_head, optimizer='nadam'):
+    model = Sequential([model, head])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['binary_accuracy'])
+    return model
 
 def replace_head(base_model):
     pass
