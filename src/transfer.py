8bc202865983f354386bad0481e9a40920e58398
Zack <zack@magnotti.net>
2021-03-17 11:55:32 -0700

    cleaned up src/transfer.py

--- src/transfer.py
+++ src/transfer.py
@@ -4,15 +4,13 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Dropout
 from tensorflow.keras.activations import swish
 from tensorflow.keras.models import load_model
+from tensorflow_addons.losses import SigmoidFocalCrossEntropy as Focal
 
-default_optimizer_ = 'adam'
-default_metrics_ = [
-    metrics.BinaryAccuracy(name='accuracy'),
-    metrics.Precision(name='precision'),
-    metrics.Recall(name='recall'),
-]
+OPTIMIZER = 'adam'
 
-onehot_metrics = [
+LOSS = Focal()
+
+METRICS = [
     metrics.CategoricalAccuracy(name='accuracy'),
     metrics.Precision(name='player_precision', class_id=0),
     metrics.Recall(name='player_recall', class_id=0),
@@ -20,26 +18,7 @@ onehot_metrics = [
     metrics.Recall(name='nonplayer_recall', class_id=1),
 ]
 
-standard_head = Sequential([
-    
-    # dense cell 1
-    Dense(128, name='head_dense_1'),
-    BatchNormalization(name='head_batchnorm_1'),
-    Activation(swish, name='head_activation_1'),
-    Dropout(.5, name='head_dropout_1'),
-    
-    # dense cell 2
-    Dense(128, name='head_dense_2'),
-    BatchNormalization(name='head_batchnorm_2'),
-    Activation(swish, name='head_activation_2'),
-    Dropout(.5, name='head_dropout_2'),
-    
-    # final output layer
-    Dense(1, activation='sigmoid', name='output')
-    
-], name='sigmoid_binary_classifier')
-
-onehot_head = Sequential([
+HEAD = Sequential([
     
     # dense cell 1
     Dense(128, name='head_dense_1'),
@@ -56,7 +35,7 @@ onehot_head = Sequential([
     # final output layer
     Dense(2, activation='softmax', name='output')
     
-], name='onehot_binary_classifier')
+], name='Binary-Classifier')
 
 def remove_head(
         base_model, 
@@ -71,46 +50,36 @@ def remove_head(
             break
     else:
         raise ValueError('base_model has no flatten layer')
+    
     name = base_model.name
-    base_model = Sequential(base_model.layers[:head_start], name=name)
-    base_model.trainable = trainable
-    return base_model
+    model = Sequential(base_model.layers[:head_start], name=name)
+    model.trainable = trainable
+    return model
 
 def add_new_head(
         base_model,
-        head = standard_head,
         name = 'transfer_model',
-        optimizer = default_optimizer_,
-        loss = 'binary_crossentropy',
-        metrics = default_metrics_
+        head = HEAD,
+        optimizer = OPTIMIZER,
+        loss = LOSS,
+        metrics = METRICS
     ):
 
-    base_model = Sequential([base_model, head], name=name)
-    base_model.compile(optimizer, loss, metrics)
-    base_model.build(input_shape=(None, None, 13))
-    return base_model
+    model = Sequential([base_model, head], name=name)
+    model.compile(optimizer, loss, metrics)
+    model.build(input_shape=(None, None, 13))
+    return model
 
 def replace_head(
         base_model,
-        head = standard_head,
         name = 'transfer_model',
-        trainable_base = False,
-        optimizer = default_optimizer_,
-        loss = 'binary_crossentropy',
-        metrics = default_metrics_
+        head = HEAD,
+        optimizer = OPTIMIZER,
+        loss = LOSS,
+        metrics = METRICS,
+        trainable_base = False
     ):
 
-    base_model = remove_head(base_model, trainable_base)
-    base_model = add_new_head(base_model, head, name, optimizer, loss, metrics)
-    return base_model
-
-def ssbml_transfer_model(
-        head = standard_head,
-        name = 'SSBML-Transfer-Model',
-        base_model = 'models/SSBML-Base-Model',
-        trainable_base = False,
-        optimizer = default_optimizer_,
-        loss = 'binary_crossentropy',
-        metrics = default_metrics_
-    ):
-    return replace_head(load_model(base_model), head, name, trainable_base, optimizer, loss, metrics)
+    model = remove_head(base_model, trainable_base)
+    model = add_new_head(model, name, head, optimizer, loss, metrics)
+    return model
